91dff30de086895c8bb4f677d7b4f060568141da
Herbert Nascimento <123819497+HACN85@users.noreply.github.com>
2023-11-23 20:19:15 +0000

    Update Supermarket.py

--- Supermarket.py
+++ Supermarket.py
@@ -43,59 +43,56 @@ if selected_product_line:
 # Display a title above the buttons
 st.title("Supermarket Sales Data Analysis")
 
-# Set up columns for button layout
-col1, col2 = st.columns(2)
+# Layout for buttons and content
+button_col1, content_col1 = st.columns([1, 4])
 
 # Buttons for Sales and Habits side by side
-with col1:
+with button_col1:
     if st.button("Sales"):
         selected_tab = "Sales"
-    else:
-        selected_tab = "None"
-
-with col2:
-    if st.button("Habits"):
+    elif st.button("Habits"):
         selected_tab = "Habits"
     else:
         selected_tab = "None"
 
 # Content based on selected button
-if selected_tab == "Sales":
-    # Display visualizations using columns
-    col3, col4, col5 = st.columns(3)
+with content_col1:
+    if selected_tab == "Sales":
+        # Display visualizations using columns
+        col3, col4, col5 = st.columns(3)
 
-    # Visualization 1: Billing per Day
-    fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Billing per Day")
-    col3.plotly_chart(fig_date, use_container_width=True)
+        # Visualization 1: Billing per Day
+        fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Billing per Day")
+        col3.plotly_chart(fig_date, use_container_width=True)
 
-    # Visualization 2: Billing per Product
-    fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", title="Billing per Product", orientation="h")
-    col4.plotly_chart(fig_prod, use_container_width=True)
+        # Visualization 2: Billing per Product
+        fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", title="Billing per Product", orientation="h")
+        col4.plotly_chart(fig_prod, use_container_width=True)
 
-    # Visualization 3: Billing by City
-    city_total_sales = df_filtered.groupby("City")[["Total"]].sum().reset_index()
-    fig_city = px.bar(city_total_sales, x="City", y="Total", title="Billing by City")
-    col5.plotly_chart(fig_city, use_container_width=True)
+        # Visualization 3: Billing by City
+        city_total_sales = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+        fig_city = px.bar(city_total_sales, x="City", y="Total", title="Billing by City")
+        col5.plotly_chart(fig_city, use_container_width=True)
 
-    st.write("---")
+        st.write("---")
 
-    # Display a title above the table
-    st.title("Table")
+        # Display a title above the table
+        st.title("Table")
 
-    # Display the DataFrame table at the bottom
-    st.write(df_filtered)
+        # Display the DataFrame table at the bottom
+        st.write(df_filtered)
 
-elif selected_tab == "Habits":
-    # Display visualizations using columns
-    col3, col4 = st.columns(2)
+    elif selected_tab == "Habits":
+        # Display visualizations using columns
+        col3, col4 = st.columns(2)
 
-    # Visualization for Habits Analysis
-    st.title("Habits Analysis")
+        # Visualization for Habits Analysis
+        st.title("Habits Analysis")
 
-    habits_data = df_filtered.groupby('Gender')['Total'].sum().reset_index()
-    habits_chart = px.bar(habits_data, x='Gender', y='Total', title='Consumption Comparison by Gender')
-    col3.plotly_chart(habits_chart, use_container_width=True)
+        habits_data = df_filtered.groupby('Gender')['Total'].sum().reset_index()
+        habits_chart = px.bar(habits_data, x='Gender', y='Total', title='Consumption Comparison by Gender')
+        col3.plotly_chart(habits_chart, use_container_width=True)
 
-    st.write("---")
-    st.title("Table for Habits Analysis")
-    st.write(df_filtered)  # Display the filtered data in a table for habits analysis
+        st.write("---")
+        st.title("Table for Habits Analysis")
+        st.write(df_filtered)  # Display the filtered data in a table for habits analysis
