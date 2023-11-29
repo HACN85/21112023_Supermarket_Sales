aefc7292525051132eb957a9954e962a581ebdb5
Herbert Nascimento <123819497+HACN85@users.noreply.github.com>
2023-11-29 17:23:14 +0000

    Update Supermarket.py

--- Supermarket.py
+++ Supermarket.py
@@ -2,94 +2,122 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Set Streamlit page configuration to wide layout
-st.set_page_config(layout="wide")
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv("supermarket_sales - Sheet1.csv")
 
-# Convert 'Date' column to datetime
-df['Date'] = pd.to_datetime(df['Date']).dt.date
-df = df.sort_values("Date")
+# Function for the Sales page
+def sales_page(df):
+    # Display a title for the Sales page
+    st.title("Sales")
 
-# Extract the year and create a new column 'Year'
-df['Year'] = pd.to_datetime(df['Date']).dt.year
-
-# Extract the month and create a new column 'Month' in 'YYYY-MM' format
-df['Month'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m')
-
-# Prepend 'All' option to the unique months
-unique_months = ['All'] + df['Month'].unique().tolist()
-
-# Create a sidebar selectbox to choose a month with 'All' option
-selected_month = st.sidebar.selectbox("Month", unique_months)
-
-# Add new sidebar options for City, Gender, and Product Line
-selected_city = st.sidebar.multiselect("City", df['City'].unique())
-selected_gender = st.sidebar.multiselect("Gender", df['Gender'].unique())
-selected_product_line = st.sidebar.multiselect("Product Line", df['Product line'].unique())
-
-# Filter the DataFrame based on the selected options
-df_filtered = df
-if selected_month != 'All':
-    df_filtered = df_filtered[df_filtered["Month"] == selected_month]
-if selected_city:
-    df_filtered = df_filtered[df_filtered["City"].isin(selected_city)]
-if selected_gender:
-    df_filtered = df_filtered[df_filtered["Gender"].isin(selected_gender)]
-if selected_product_line:
-    df_filtered = df_filtered[df_filtered["Product line"].isin(selected_product_line)]
-
-# Display a title above the tabs
-st.title("Supermarket Sales Data Analysis")
-
-# Create tabs for "Sales" and "Habits"
-tabs = ["Sales", "Habits"]
-selected_tab = st.sidebar.radio("Go to", tabs)
-
-if selected_tab == "Sales":
+    # Display a subheader for the Sales page
+    st.subheader("_Supermarket - Consumer purchasing habits_")
+    st.write("---")
     # Display visualizations using columns
-    col1, col2, col3 = st.columns([1, 1, 2])
+    col1, col2 = st.columns(2)
+    col3, col4, col5 = st.columns(3)
 
     # Visualization 1: Billing per Day
-    fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Billing per Day")
+    fig_date = px.bar(df, x="Date", y="Total", color="City", title="Billing per Day")
     col1.plotly_chart(fig_date, use_container_width=True)
 
     # Visualization 2: Billing per Product
-    fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", title="Billing per Product", orientation="h")
+    fig_prod = px.bar(df, x="Date", y="Product line", color="City", title="Billing per Product", orientation="h")
     col2.plotly_chart(fig_prod, use_container_width=True)
 
     # Visualization 3: Billing by City
-    city_total_sales = df_filtered.groupby("City")[["Total"]].sum().reset_index()
+    city_total_sales = df.groupby("City")[["Total"]].sum().reset_index()
     fig_city = px.bar(city_total_sales, x="City", y="Total", title="Billing by City")
     col3.plotly_chart(fig_city, use_container_width=True)
 
+    st.write("---")
+
     # Visualization 4: Billing by type of Payment
-    col4, col5 = st.columns([1, 1])
-    fig_type = px.pie(df_filtered, values="Total", names="Payment", title="Billing by type of Payment")
+    fig_type = px.pie(df, values="Total", names="Payment", title="Billing by type of Payment")
     col4.plotly_chart(fig_type, use_container_width=True)
 
     # Visualization 5: Evaluation by City
-    city_total_ratings = df_filtered.groupby("City")[["Rating"]].mean().reset_index()
+    city_total_ratings = df.groupby("City")[["Rating"]].mean().reset_index()
     fig_rating = px.bar(city_total_ratings, x="Rating", y="City", title="Evaluation by City")
     col5.plotly_chart(fig_rating, use_container_width=True)
 
-    st.write("---")
-
-    # Display a title above the table
-    st.title("Table")
+    # Display a subtitle above the table
+    st.subheader("Table")
 
     # Display the DataFrame table at the bottom
-    st.write(df_filtered)
-
-elif selected_tab == "Habits":
-    # Visualization for Habits Analysis
-    st.title("Habits Analysis")
-
-    habits_data = df_filtered.groupby('Gender')['Total'].sum().reset_index()
-    habits_chart = px.bar(habits_data, x='Gender', y='Total', title='Consumption Comparison by Gender')
-    st.plotly_chart(habits_chart)
-
+    st.write(df)
     st.write("---")
-    st.title("Table for Habits Analysis")
-    st.write(df_filtered)  # Display the filtered data in a table for habits analysis
+
+    # Adding a footer for data source
+    st.markdown("_Source: Supermarket dataset from Kaggle's repository_")
+
+# Function for the Habits page
+def habits_page(df):
+    # Display a title for the Habits page
+    st.title("Habits")
+
+
+    # Filter dataframe for men and women separately
+    df_male = df[df['Gender'] == 'Male']
+    df_female = df[df['Gender'] == 'Female']
+
+    # Visualization 1: Purchase behavior by city for Men and Women
+    fig_city_gender = px.histogram(df, x='City', color='Gender', title='Purchase behavior by City for Men and Women')
+    st.plotly_chart(fig_city_gender, use_container_width=True)
+
+    # Visualization 2: Purchase behavior by product line for Men and Women
+    fig_product_gender = px.histogram(df, x='Product line', color='Gender',
+                                      title='Purchase behavior by Product for Men and Women')
+    st.plotly_chart(fig_product_gender, use_container_width=True)
+    st.write("---")
+    st.markdown("_Source: Supermarket dataset from Kaggle's repository_")
+
+
+
+# Main function to handle navigation between pages
+def main():
+    # Set Streamlit page configuration to wide layout
+    st.set_page_config(layout="wide")
+
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv("supermarket_sales - Sheet1.csv")
+
+    # Convert 'Date' column to datetime
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    df = df.sort_values("Date")
+
+    # Extract the year and create a new column 'Year'
+    df['Year'] = pd.to_datetime(df['Date']).dt.year
+
+    # Extract the month and create a new column 'Month' in 'YYYY-MM' format
+    df['Month'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m')
+
+    # Prepend 'All' option to the unique months
+    unique_months = ['All'] + df['Month'].unique().tolist()
+
+    # Sidebar menu options
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ("Sales", "Habits"))
+
+    # Sidebar menu for selecting month, city, and gender
+    selected_month = st.sidebar.selectbox("Month", unique_months)
+    selected_city = st.sidebar.selectbox("City", ["All"] + df["City"].unique().tolist())
+    selected_gender = st.sidebar.selectbox("Gender", ["All", "Male", "Female"])
+
+    # Filter the DataFrame based on the selected options
+    df_filtered = df.copy()
+    if selected_month != 'All':
+        df_filtered = df_filtered[df_filtered["Month"] == selected_month]
+    if selected_city != 'All':
+        df_filtered = df_filtered[df_filtered["City"] == selected_city]
+    if selected_gender != 'All':
+        df_filtered = df_filtered[df_filtered["Gender"] == selected_gender]
+
+    # Display the selected page
+    if page == "Sales":
+        sales_page(df_filtered)
+    elif page == "Habits":
+        habits_page(df_filtered)
+
+
+if __name__ == "__main__":
+    main()
